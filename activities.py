1d43334dc1c8bbf68b42e33094f60c83e6afce90
Erin Polley <erinepolley@gmail.com>
2020-01-14 15:56:43 -0600

    finished exercise

---
+++ activities.py
@@ -0,0 +1,23 @@
+running_kids = ["Pam", "Sam", "Andrea", "Will"]
+swinging_kids = ["Marybeth", "Jenna", "Kevin", "Courtney"]
+sliding_kids = ["Mike", "Jack", "Jennifer", "Earl"]
+hiding_kids = ["Henry", "Heather", "Hayley", "Hugh"]
+#Need to loop over the above lists
+for kid in running_kids:
+    print(f"{kid} ran so fast today.")
+
+for kid in swinging_kids:
+# def swing(kid):
+    print(f"{kid} loves to swing.")
+
+for kid in sliding_kids:
+# def slide(child):
+    print(f"{kid} loves to slide.")
+
+for kid in hiding_kids:
+    print(f"I played hide and seek with {kid} today.")
+
+# (run("Georgia"))
+# (swing("Isabel"))
+# (slide("Melody"))
+# (hide_and_seek("Sam"))
